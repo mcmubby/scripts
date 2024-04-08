ecadcbdcd3d9ec4a217dd074203f5426c2643d7b
mcmubby <12859241+mcmubby@users.noreply.github.com>
2024-04-08 13:44:21 +0100

    dynamic payload

--- jira-w-github/create_issues.py
+++ jira-w-github/create_issues.py
@@ -3,7 +3,7 @@ from requests.auth import HTTPBasicAuth
 from dotenv import load_dotenv
 import json
 import os
-from flask import Flask
+from flask import Flask, request
 
 app = Flask(__name__)
 
@@ -12,58 +12,80 @@ load_dotenv()
 jira_domain = os.getenv('jira_domain')
 api_token = os.getenv('jira_github_token')
 email = os.getenv('jira_email')
+project_key = 'GI'
+trigger_comment = '/jira'
 
-# Define a route that handles GET requests
+
 @app.route('/createJira', methods=['POST'])
 def createJira():
 
-    url = f"https://{jira_domain}/rest/api/3/issue"
+    try:
+        ## Parse request payload. Ideally handle error
+        data = request.get_json()
 
-    auth = HTTPBasicAuth(email, api_token)
+        if((data['comment']['body']).lower() == trigger_comment):
+            ## Auth data
+            url = f"https://{jira_domain}/rest/api/3/issue"
 
-    headers = {
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
+            auth = HTTPBasicAuth(email, api_token)
 
-    payload = json.dumps( {
-        "fields": {
-        "description": {
-            "content": [
-                {
+            headers = {
+                "Accept": "application/json",
+                "Content-Type": "application/json"
+            }
+
+
+            ## Jira Ticket Payload
+            payload = json.dumps( {
+                "fields": {
+                "description": {
                     "content": [
                         {
-                            "text": "Order entry fails when selecting supplier.",
-                            "type": "text"
-                        }
-                    ],
-                    "type": "paragraph"
-                    }
-                ],
-            "type": "doc",
-             "version": 1
-        },
-        "project": {
-           "key": "GI"
-        },
-        "issuetype": {
-            "id": "10009"
-        },
-        "summary": "Main order flow broken",
-    },
-    "update": {}
-    } )
+                            "content": [
+                                {
+                                    "text": data['issue']['body'],
+                                    "type": "text"
+                                }
+                            ],
+                            "type": "paragraph"
+                            }
+                        ],
+                    "type": "doc",
+                     "version": 1
+                },
+                "project": {
+                   "key": project_key
+                },
+                "issuetype": {
+                    "id": "10009"
+                },
+                "summary": data['issue']['title'],
+            },
+            "update": {}
+            } )
 
 
-    response = requests.request(
-        "POST",
-        url,
-        data=payload,
-        headers=headers,
-        auth=auth
-    )
+            response = requests.request(
+                "POST",
+                url,
+                data=payload,
+                headers=headers,
+                auth=auth
+            )
 
-    return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+            return json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": "))
+        
+        else:
+            return 'No action', 200
+    
+    ## Recommended to handle specific errors like empty request payload, post request to jira api failure...
+    ## Rather than the blanket exception below
+    except Exception as e:
+        error_response = {
+            'status': 'error',
+            'message': str(e)
+        }
+        return (error_response), 500
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
